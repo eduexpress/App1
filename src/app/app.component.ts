f1fc17c0230ba61bda9f7dfff7149206cd0b918f
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-05-26 11:39:08 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-sample',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'App1';
+
+  loginForm = new FormGroup({
+    email: new FormControl(
+      '',[
+        Validators.email,
+        Validators.required
+        ]
+    ),
+    password: new FormControl(
+      '',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(10)
+      ]
+    )
+  });
+
+  login(){
+    console.log(this.loginForm)
+    console.log(this.loginForm.get('email'))
+    console.log(this.loginForm.get('password'))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SampleComponent} from './ui/sample/sample.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule/*two way data binding*/,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
